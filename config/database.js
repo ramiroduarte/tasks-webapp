0f92ceecfd667899193d23cf78bb98a7d2d04890
Ramiro <ramiroduarte2408@gmail.com>
2025-03-17 22:55:23 -0300

    Fix bug

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose'
 import chalk from 'chalk'
 import ora from 'ora'
+import 'dotenv/config';
 
 console.log(chalk.yellow('----------------------------------------'))
 const spinner = ora(chalk.cyan('Database is connecting...')).start();
-mongoose.connect('mongodb://127.0.0.1:27017/tasks-webapp')
+mongoose.connect(process.env.MONGODB_URI)
     .then(db => {
         spinner.succeed(`Database is ${chalk.green('connected')}! ${chalk.cyan(`(${db.connections[0].name})`)}`)
         console.log(chalk.yellow('----------------------------------------\n'))
